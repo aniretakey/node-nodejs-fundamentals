0b1f3bc04408fd312b1f39a4218a4c00e8afbda1
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-05 19:50:54 +0300

    start function restore

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,46 @@
+import { readdir } from 'fs/promises'
+import { fileURLToPath } from "url";
+import { dirname, join, parse } from "path";
+
+const recursiveFileSearch = async (dir) => {
+    const entries = await readdir(dir, {withFileTypes: true});
+
+    for (const entry of entries) {
+        if (!entry.isDirectory() && entry.name === 'snapshot.json') {
+            return join(dir, entry.name);
+        }
+    }
+
+    for (const entry of entries) {
+        if (entry.isDirectory()) {
+            const innerPath = join(dir, entry.name);
+            const found = await recursiveFileSearch(innerPath);
+
+            if (found !== null) {
+                return found;
+            }
+        }
+    }
+
+    return null;
+}
+
+
 const restore = async () => {
-  // Write your code here
-  // Read snapshot.json
-  // Treat snapshot.rootPath as metadata only
-  // Recreate directory/file structure in workspace_restored
+    const __filename = fileURLToPath(import.meta.url, fileURLToPath(import.meta.url));
+    const __dirname = dirname(__filename);
+    const startDir = parse(__dirname).dir
+
+    console.log('startDir', startDir)
+
+    let foundJson = await recursiveFileSearch(startDir);
+    console.log('foundJson', foundJson)
+
+    if (!foundJson) {
+        throw new Error('FS operation failed');
+    }
+
+
 };
 
 await restore();
