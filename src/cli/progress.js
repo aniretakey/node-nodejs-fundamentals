dbb4c74917466cb38c3e36626d3c37c27d92d249
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-06 19:07:47 +0300

    delete comment & console.log

--- src/cli/progress.js
+++ src/cli/progress.js
@@ -5,16 +5,11 @@ const getProgressBarArgs = () => {
     const args = process.argv.slice(2);
     let duration = 5000, interval = 100, length = 30, color = null;
 
-    console.log('args', args)
-
     for (let i = 0; i < args.length; i++) {
         const currElem = args[i];
         const nextElem = args[i + 1];
 
         if (currElem && nextElem) {
-            console.log('currElem', currElem)
-            console.log('nextElem', nextElem)
-
             if (currElem === '--duration') {
                 duration = Number(nextElem);
             }
@@ -37,15 +32,8 @@ const getProgressBarArgs = () => {
 }
 
 const progress = () => {
-    // Write your code here
-    // Simulate progress bar from 0% to 100% over ~5 seconds
-    // Update in place using \r every 100ms
-    // Format: [████████████████████          ] 67%
     const {duration, interval, length, color} = getProgressBarArgs();
 
-    console.log('duration', duration);
-    console.log('interval', interval);
-    console.log('length', length);
     console.log('color', color);
 
     const startTime = Date.now();
