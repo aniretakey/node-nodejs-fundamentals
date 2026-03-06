14261a07d475a4bbf15ed9ae7ec1cdad1a9fe1ed
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-06 19:07:19 +0300

    add function for paint progress bar

--- src/cli/progress.js
+++ src/cli/progress.js
@@ -16,7 +16,7 @@ const getProgressBarArgs = () => {
             console.log('nextElem', nextElem)
 
             if (currElem === '--duration') {
-                duration = nextElem;
+                duration = Number(nextElem);
             }
 
             if (currElem === '--interval') {
@@ -33,7 +33,7 @@ const getProgressBarArgs = () => {
         }
     }
 
-    return {duration, interval, length};
+    return {duration, interval, length, color};
 }
 
 const progress = () => {
@@ -48,6 +48,25 @@ const progress = () => {
     console.log('length', length);
     console.log('color', color);
 
+    const startTime = Date.now();
+
+    const timer = setInterval(() => {
+        const timePassed = Date.now() - startTime;
+        const progressPercent = Math.min((timePassed / duration) * 100, 100);
+
+        const filledBlocks = Math.floor((progressPercent / 100) * length);
+        const emptyBlocks = length - filledBlocks;
+
+        const progressString = `[${FILLED_VALUE.repeat(filledBlocks)}${EMPTY_VALUE.repeat(emptyBlocks)}] ${Math.floor(progressPercent)}%`;
+
+        process.stdout.write(`\r${progressString}`)
+
+        if (progressPercent >= 100) {
+            process.stdout.write('Done!');
+            clearInterval(timer);
+        }
+
+    })
 };
 
 progress();
