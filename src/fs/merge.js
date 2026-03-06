637a5da39c858ff2b7e8f49df5a547ea08a3d7e4
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-06 20:27:42 +0300

    add folder search

--- src/fs/merge.js
+++ src/fs/merge.js
@@ -1,8 +1,33 @@
+import { readdir } from 'fs/promises';
+import { join } from 'path';
+
+const recursiveFolderSearch = async (dir) => {
+    const entries = await readdir(dir, {withFileTypes: true});
+
+    for (const entry of entries) {
+        if (entry.isDirectory() && entry.name === 'workspace') {
+            return join(dir, entry.name);
+        }
+    }
+
+    for (const entry of entries) {
+        if (entry.isDirectory() && entry.name !== 'workspace') {
+            const innerPath = join(dir, entry.name);
+            const found = await recursiveFolderSearch(innerPath);
+            if (found !== null) {
+                return found;
+            }
+        }
+    }
+
+    return null;
+};
+
 const merge = async () => {
-  // Write your code here
-  // Default: read all .txt files from workspace/parts in alphabetical order
-  // Optional: support --files filename1,filename2,... to merge specific files in provided order
-  // Concatenate content and write to workspace/merged.txt
+    // Write your code here
+    // Default: read all .txt files from workspace/parts in alphabetical order
+    // Optional: support --files filename1,filename2,... to merge specific files in provided order
+    // Concatenate content and write to workspace/merged.txt
 };
 
 await merge();
